e640b053ba99f20b19bf56e945b5d16aa3013076
Sabarish Raghupathy <sabarishraghupathy@gmail.com>
2020-05-16 02:00:26 -0400

    Updated build location to . and dist

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,6 +1,8 @@
 const sh = require('shelljs');
 const upath = require('upath');
 
-const destPath = upath.resolve(upath.dirname(__filename), '../docs');
+const destPath = upath.resolve(upath.dirname(__filename), '../dist');
 
 sh.rm('-rf', `${destPath}/*`)
+
+sh.rm('-rf', `${destPath}/../index.html`)
